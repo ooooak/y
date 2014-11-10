f7f98902b460433b2dfb01bc3f93d4ee4ae4cab6
Akshay <akshay.deep0@gmail.com>
2014-11-10 21:40:46 +0530

    fixed percentage bug

--- yask.php
+++ yask.php
@@ -1,7 +1,6 @@
 <?php
-
-// error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
+/*
+*/
 class Yask
 {
 	/**
@@ -9,15 +8,10 @@ class Yask
 	*/
 	private static $stats = [];
 
-	/**
-	* Microseconds per 100 millisecond 
-	*/
-    private static $MS_PER_100MS = 100000; // 1,000,00
-
     /**
 	* Microseconds per second
 	*/    
-    private static $MS_PER_SECOND = 1000000; // 10,00,000
+    private static $MS = 1000000; // 10,00,000
 
 	/**
 	* @param $name [String] Benchmark Name. It will be display in report.
@@ -36,18 +30,25 @@ class Yask
 	{
 		$stats = self::$stats;
 
-		usort($stats, ['self', 'usort_callback']);
+		if (empty($stats))
+		{
+			return;
+		}
 
-		$best = $stats[0]['time'];
+		usort($stats, ['self', 'usort_callback']);
 
 		foreach ($stats as $stat) 
 		{
-			self::put($stat['name'], ': ', self::make_time($best, $stat['time']));
+			self::put($stat['name'], ': ', self::make_time($stats[0]['time'], $stat['time']));
 		}
 
-		// do we really need this ?
-		self::$stats = [];
+		self::$stats = []; // reset stats
 	}
+
+
+	// ------------------------------------------
+	// 				Private Methods 
+	// ------------------------------------------
 
 	private static function usort_callback($item, $next_item)
 	{
@@ -58,11 +59,6 @@ class Yask
 		
 	    return ($item['time'] < $next_item['time']) ? -1 : 1;
 	}
-
-
-	// ------------------------------------------
-	// 				Private Methods 
-	// ------------------------------------------
 
 	/**
 	* call user callback and record time.
@@ -94,19 +90,16 @@ class Yask
 		return $end - $start;
 	}
 
-	/**
-	* TODO: test loop count
-	*/
 	private static function count_time_with_loop(callable $func, $loop)
 	{	
-		$time = 0;
+		$total = 0;
 
-		for ($i=0; $i < $loop; $i++)
+		for ($i=1; $i <= $loop; $i++)
 		{ 
-			$time += self::count_time($func);
+			$total += self::count_time($func);
 		}
 
-		return $time;
+		return $total;
 	}
 
 
@@ -127,16 +120,14 @@ class Yask
 		echo PHP_EOL;
 	}
 
-	/* TODO: maybe convert time from float to int ms
-	* then get percentage
+	/*
+	* Then get percentage
 	*/
 	private static function make_time($best, $time)
 	{
-		// $_time = round($time * 1000000); time:$_time
-
 		$per = self::get_percentage($best, $time);
 
-		return round($time * self::$MS_PER_SECOND) . "ms (slower:$per%)";
+		return round($time * self::$MS) . "ms (slower:$per%)";
 	}
 
 	/**
@@ -144,10 +135,8 @@ class Yask
 	* @param $best [float] best time
 	*/
 	private static function get_percentage($best, $time)
-	{
-		// return 100 - ($best/$time) * 100;
-
-		return ($best == $time) ? 0 : round(100 - ($best/$time) * 100);
+	{	
+		return ($best == $time) ? 0 : (($time-$best)/$best) * 100;
 	}
 
 	/**
